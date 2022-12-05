339fbacf95acfb6a034e56d07daaa7e664bedd2c
hashqueue <1912315910@qq.com>
2022-12-05 09:57:21 +0800

    feat: add 前端动态路由功能.

--- candy/settings.py
+++ candy/settings.py
@@ -98,10 +98,10 @@ REST_FRAMEWORK = {
     # 指定drf使用的过滤后端
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     # 接口限流
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.AnonRateThrottle',
+    #     'rest_framework.throttling.UserRateThrottle'
+    # ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
         'user': '2000/day'
