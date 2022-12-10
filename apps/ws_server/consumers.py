31f4d985b105c55af8141f51cef1221204733d35
hashqueue <1912315910@qq.com>
2022-12-11 03:19:45 +0800

    perf: 优化ws获取服务器性能数据逻辑.

--- apps/ws_server/consumers.py
+++ apps/ws_server/consumers.py
@@ -14,22 +14,22 @@ class ServerPerformanceConsumer(JsonWebsocketConsumer):
             available_memory = eval(f'{(virtual_memory.available / 1024 / 1024 / 1024):.2f}')
             free_memory = eval(f'{(virtual_memory.free / 1024 / 1024 / 1024):.2f}')
             used_memory = eval(f'{((virtual_memory.total - virtual_memory.free) / 1024 / 1024 / 1024):.2f}')
-            percent_memory = eval(
+            memory_percent = eval(
                 f'{((virtual_memory.total - virtual_memory.available) / virtual_memory.total * 100):.2f}')
             disk_info = psutil.disk_usage('/')
             total_disk = eval(f'{disk_info.total / 1024 / 1024 / 1024:.2f}')
             used_disk = eval(f'{disk_info.used / 1024 / 1024 / 1024:.2f}')
             free_disk = eval(f'{disk_info.free / 1024 / 1024 / 1024:.2f}')
-            percent_disk = disk_info.percent
+            disk_percent = disk_info.percent
             server_data = {
                 'cpu': {'cpu_count': cpu_count, 'cpu_percent': cpu_percent},
                 'memory': {
                     'total_memory': total_memory, 'available_memory': available_memory, 'free_memory': free_memory,
-                    'used_memory': used_memory, 'percent_memory': percent_memory
+                    'used_memory': used_memory, 'memory_percent': memory_percent
                 },
                 'disk': {
                     'total_disk': total_disk, 'used_disk': used_disk, 'free_disk': free_disk,
-                    'percent_disk': percent_disk
+                    'disk_percent': disk_percent
                 }
             }
             self.send_json({"data": server_data})
